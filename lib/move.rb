73594140ebfe3a582ae32886856bcf00e2eb6170
miclp <michaelpeimani@gmail.com>
2016-02-16 19:39:00 -0800

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,8 @@ def display_board(board)
 end
 
 # code your move method here!
+def move(board , location , player_character = "X")
+  location = location.to_i
+  board[location-1] = player_character
+  return board
+end
