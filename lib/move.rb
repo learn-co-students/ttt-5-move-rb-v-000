65855430ba3eaabb2e340b6261b182c928fad492
Seunghwan Kim <enchershkim@hotmail.com>
2016-02-19 17:35:35 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board(board=[" "," "," "," "," "," "," "," "," "])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -7,3 +7,7 @@ def display_board(board)
 end
 
 # code your move method here!
+def move(board, input, placeholder='X')
+  board[input.to_i-1] = placeholder
+  board
+end
