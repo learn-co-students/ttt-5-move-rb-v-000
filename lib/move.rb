a1f1a2c35cdcd4329643c1ad4b9d7b6d1c6c5b1e
Scott Wakeman <swakeman@ca.rr.com>
2016-05-27 09:05:21 -0700

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,26 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+user_input.to_i - 1
+end
+
+def move
+end
+
+def move(array, index, value)
+end
+
+def move(array, index, value="X")
+end
+
+def move(array, index, value="X")
+  array[index] = value
+end
+
+board = ["", "", ""]
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+
+update_array_at_with(board, 0, "Red")
