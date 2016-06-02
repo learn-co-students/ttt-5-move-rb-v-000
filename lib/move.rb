f8aea19e49318f62c420746b4ce8711f7c3cd19b
treverandersen <trever.r.andersen@gmail.com>
2016-06-01 23:59:45 -0500

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -13,13 +13,15 @@ user_space = [1,2,3,4,5,6,7,8,9]
 user_char = "X","O"
 
 def input_to_index(user_space)
-	user_space.to_i - 1 
+	user_space.to_i - 1
 end
 
-input_to_index(4)
 
 def move(board, user_space, user_char="X")
-	board[0] = "X"
+	board[user_space] = user_char 
 end
 
-move(board, 2)
+move(board, 2, "O")
+move(board, 8)
+move(board, 0)
+move(board, 4)
