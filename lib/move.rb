70b3e248a3a28e3d2af9450f32d834ce4cec8352
Leonard Bernardo <leonard.bernardo@gmail.com>
2016-07-11 23:09:27 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,18 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+
+### code your input_to_index and move method here!
+# code input_to_index code
+def input_to_index(input)
+  # input.to_i - 1  Another way of writing this code block
+input = input.to_i
+input = input - 1
+end
+
+# code move method
+### top left with X
+
+def move(board,position,token = "X")
+  board[position] = token
+end
