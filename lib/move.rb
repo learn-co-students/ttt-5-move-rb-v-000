fdb31b4d198a727e4e95aef6742f0eb987dd2ce8
Victoria Sowell <tpms123@gmail.com>
2017-01-30 04:01:19 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,15 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+  input = input.to_i
+  index = input - 1
+end
+
+def move(array, index, value)
+  array[index] = value
+end
+
+def greeting(name)
+  puts "Hi, #{name}! Welcome to Tic Tac Toe!"
+end
