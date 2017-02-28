78617ef201d61ae18286149b11fef8672fd2af22
Kerri Resing <kerri.resing@gmail.com>
2017-02-28 22:20:22 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board, input_to_index, character = "X")
-  board[input_to_index] = character
+def move(board, index, character = "X")
+  board[index] = character
 end
