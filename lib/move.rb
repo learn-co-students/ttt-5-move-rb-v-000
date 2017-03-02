4243f0c6d4780456892eafbcc5bfaceb0ed3bdb9
Beth Turner <englishturner@gmail.com>
2017-03-02 19:59:26 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,15 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+#input is what they type in and we convert it
+def input_to_index(input)
+  # convert input to integer and subtract 1.
+  #user's input ("1"-"9") and convert it to the index of the board array (0-8)
+  input.to_i - 1
+  # users count from 1, Ruby from 0
+end
+
+def move(board, index, value = 'X')
+  #index = input_to_index(input)
+board[index] = value
+end
