c888d04963d4548c17354048484ba38ccf2d53d4
David Mars <david.w.mars@my.occc.edu>
2017-03-20 06:22:37 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board(board)           #Sets array locations to corresponding board locations
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -7,3 +7,10 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)  #Get user move, -1 for proper array location
+  user_input.to_i - 1
+end
+
+def move(board, position, character = "X")
+  board[position] = character             #set array location = to player char
+end
