790f155fda85f8d69a70296ccd3f46c251280484
Casey Tyack <kazenorite@gmail.com>
2017-04-25 18:01:56 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -5,5 +5,13 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
+  
 # code your input_to_index and move method here!
+
+def input_to_index(aMove)
+  output1 = aMove.to_i - 1
+end
+
+def move(boardArray, squareSelected, xOrO = "X") 
+  boardArray[squareSelected] = xOrO
+end
