b387593452131a1c19f568de13fe6d308f58c97b
Sheila O'Donnell <sheila@contexttravel.com>
2017-05-13 21:58:16 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,38 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
+
 
 # code your input_to_index and move method here!
+
+def input_to_index(user_input)
+  (user_input).to_i-1
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
+
+#
+# def input_to_index(user_input)
+#   user_input = gets.strip
+
+# puts "Welcome to Tic Tac Toe!"
+# puts "Where would you like to go?"
+# def input_to_index[user_input]
+#   user_input = gets.strip
+#   [user_input].to_i - 1
+# end
+#
+# def move(board, [user_input], character)
+#   character = "X"
+# end
+# def display_board(board)
+#   puts [" "," "," "," "," "," "," "," "," "]
+# end
