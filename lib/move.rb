e5a78e59eba419337c5bc2961cad817b242464c8
Alyssa Ragusa <alyssa.ragusa07@yahoo.com>
2017-07-08 03:11:01 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,19 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+def display_board(game_board)
+  puts " #{game_board[0]} | #{game_board[1]} | #{game_board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{game_board[3]} | #{game_board[4]} | #{game_board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{game_board[6]} | #{game_board[7]} | #{game_board[8]} "
 end
 
-# code your input_to_index and move method here!
+def move(board_array, index, character = "X")
+  board_array[index] = character
+end
+
+def input_to_index(number)
+  number.to_i - 1
+end
+
+def greeting
+  puts "Welcome to Tic Tac Toe!"
+end
