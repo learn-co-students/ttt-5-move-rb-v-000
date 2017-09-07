8163e49a68fcb4ef5a7ee7e017f09729b084e9e3
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-09-07 03:07:42 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,30 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 # code your input_to_index and move method here!
+def input_to_index(input)
+  s = input.to_i
+  s - 1
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+end
+
+
+
+
+
+  #puts "Turn 1: Pick 1 - 9"
+
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+
+#position = gets.strip
+#token = "X"
+#board[position.to_i] = token
+#end
+
+#def input_to_index(board)
+#  puts board.to_i - 1
+#end
