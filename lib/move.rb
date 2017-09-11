2362e90b1cc9f379c6859a2196a6e78e19f08bfd
Katie Cassell <kcassell944@gmail.com>
2017-09-11 23:41:09 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,27 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+def input_to_index(input)
+ index = input.to_i - 1
 end
+
+def move(array, index, token = "X")
+  # array is our array of tokens "X", "O", " "
+  # we need to update our array at the point of the index to that character(token)
+
+  array[index] = token
+end
+
+# move(board, 0, "O")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # code your input_to_index and move method here!
