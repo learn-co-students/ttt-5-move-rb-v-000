fd855e96e7a9707250314778bf052599cc33f9f2
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-05 20:45:27 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,14 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index
+
+end 
+
+
+def move(board, position, char = "X")
+  #place character at index 0 of board array
+  board[0] = char
+  
+  
+end
