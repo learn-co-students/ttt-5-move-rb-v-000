70604ef112c2a90820530e4965423c0cec3571f3
github username <github email address>
2017-10-11 08:28:24 -0400

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -8,11 +8,11 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(input)
-  return input.to_i - 1
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
 
-def move(board,index,character="X")
-  board[index] = character
+def move(board,index,current_player="X")
+  board[index] = current_player
 end
