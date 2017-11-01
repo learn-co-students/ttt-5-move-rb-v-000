398488443cba293727516252b408a9206909c5f1
Hannah Carnes <hecarnes@gmail.com>
2017-11-01 00:07:58 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -13,7 +13,6 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def move(board, input, index = "X")
-  board[input] = index
+def move(board, index, char = "X")
+  board[index] = char
 end
