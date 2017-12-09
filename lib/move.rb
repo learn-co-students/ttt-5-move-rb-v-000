3762ba007b1935920f110a32f99c98ffb4596d00
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-12-09 21:35:14 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,5 +1,20 @@
+
+board =Array.new
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def input_to_index(user_input)
+  user_input = user_input.to_i
+  return index = user_input - 1
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+  return board
+end
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  #puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts board[0]
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
