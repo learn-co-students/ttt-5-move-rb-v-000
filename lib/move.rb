ad922fcb8a3007237721cbf001790f682ba0cc41
github username <github email address>
2017-12-20 00:59:48 -0500

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(user_input)
+	return user_input.to_i - 1
+end
+
+def move(board,user_input,x_or_o = "X")
+	return board[user_input]= x_or_o
+end
