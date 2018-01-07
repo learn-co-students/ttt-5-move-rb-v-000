d6470002ca935d63d6b68c39b46f16dfbd35c16c
Kate Drennen <k8ted11@gmail.com>
2018-01-07 01:37:07 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,15 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+# # code your input_to_index and move method here!
+def input_to_index(user_input)
+  puts "Welcome to Tic Tac Toe!"
+  puts "Where would you like to go?"
+  puts "#{user_input}"
+  #1.upto(9) { |n| print n, ".."} this passes, so does user_input = 5
+
+end
+
+def move(board)
+puts "#{board[]}"
+end
