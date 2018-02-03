a1fe4310b3fdd88fd7762d3ebb2f3bbd0fcc08aa
Cedric Niguidula <cedcn-9mail@yahoo.com>
2018-02-03 02:12:53 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,9 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(uinput)
+  pos = uinput.to_i - 1
+end
+def move(board_name, bdpos, char = "X")
+  board_name[bdpos] = char
+end
