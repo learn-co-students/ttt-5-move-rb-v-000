24715fbfd381b3cb9c12c5931f4b6446f961ae0c
Jaime Johnsen <drivetraindiva@gmail.com>
2018-03-05 03:40:36 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,13 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(move)
+  index = move.to_i - 1
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+  end
+  
+  #how is update_array_at_with used? is it called board now per my code?
+#how is the players input for a move a string when they input the number at the CLI?
