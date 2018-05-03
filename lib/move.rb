42873b7dff046030a2f61ea661b65d04e6899efc
Stacy Carr <stacy@boostersinabox.com>
2018-05-03 17:06:51 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,7 @@
+# define the array for the board
+puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+# display the grid for the board with placeholders for each move
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,4 +10,18 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+# convert user input into a integer, minus 1 because the array starts with zero
+
+user_input = "6"
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+converted_input = input_to_index(user_input)
+
+# move method using array, index & X or O 
+def move(board, converted_input, user_marker = "X")
+  board[converted_input] = user_marker
+end
+
