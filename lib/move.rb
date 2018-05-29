28cd32cf320f596c5fd1cd02aca919b26b320fc4
johnkounelias@gmail.com Kounelias <johnkounelias@gmail.com>
2018-05-29 00:58:05 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,24 +1,23 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 def input_to_index(input)
-input.to_i - 1
-end
-def move(board, input, value = "X")
-#  require 'pry'
-#  binding.pry
-  board[input] = value
+  index=input.to_i
+  index - 1
 end
 
 
+def move(board, index, char="X")
+board[index]=char
+
+end
+  
+
+
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} " 
+puts                "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts                "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
 
 
 
-
-
-
-# code your input_to_index and move method here!
