2f94aef73b1672ce48cfb6c06231d43ad97e9689
Ashleigh Chun <ashleigh.chun@wework.com>
2018-07-11 05:20:41 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,27 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+
+#grab the users input and convert it to an integer
+#then subtract it by 1, to match the index numbers
+
+def input_to_index(position)
+  position.to_i - 1
+end
+
+puts "testing..."
+puts input_to_index("5")
+
+
+def move(array, index, value = "X")
+  array[index] = value
+end
+ 
+
+
+
+
+
+
+
