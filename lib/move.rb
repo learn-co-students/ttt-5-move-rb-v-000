0cf8b7e36b795a123b104075e81fe03fa31881a6
Kimberly Aviles <01010110.v@gmail.com>
2018-07-30 06:13:54 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+
+
+def input_to_index
+  converted_input = input_to_index(user_input)
+end
+
+
+
+def move
+
+end
