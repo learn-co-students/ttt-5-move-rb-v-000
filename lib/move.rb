dba55454bf75591bc3924195b163113e9b40f714
Tim Knoll <timothy.knoll@gmail.com>
2018-08-05 15:46:11 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +9,14 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index (user_input)
+# convert 1-9 move from string into integer and correlating array(index)
+  index = user_input.to_i - 1
+end
+
+
+def move (board, index, token = "X")
+  # is to update the board at the position with the value
+  board[index] = token
+  return board
+end
