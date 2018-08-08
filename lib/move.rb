a96074f8287cfcae3e2c1319c51bddf42d3f25ef
talyaflowers1@gmail.com <talyaflowers1@gmail.com>
2018-08-08 03:38:43 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,14 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index (user_input)
+  new_user_input = user_input.to_i #takes the users input and converts it into an integer
+  new_user_input -=1 #the board has nine spots available but the array will start us at zero.
+  return new_user_input #How do we store the users input ?
+end
+
+def move(board, index, character = "X")
+board[index] = character
+return board #referring to the main board
+end
