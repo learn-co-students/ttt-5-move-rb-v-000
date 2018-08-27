b6f0ff7ed98b87d10c85829e14e45113f921e067
Toni-Ty <antonique2479@yahoo.com>
2018-08-27 06:14:32 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -5,9 +5,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  def input_to_index()
-  def move (board, O, X)
-  board[O] = "X"
-end
