6d8c31f08a3b2904096663244d409bbddf0762b0
Ellen Pu <ellenpu251@gmail.com>
2018-12-10 18:06:34 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,13 @@
+def input_to_index(index)
+  index = index.to_i
+  index = index - 1
+  return index
+end
+
+def move(array,index,value = "X")
+  array[index] = value
+end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
