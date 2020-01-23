e47aa47fdcd838e167e77891bf51306613b1babb
Max Ringelheim <max.ringelheim@gmail.com>
2020-01-23 20:57:29 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -13,6 +13,6 @@ def input_to_index(user_input)
   user_input.to_i-1
 end
 
-def move(board, user_input, character = "X")
+def move(board, user_input, character = "O")
   board[user_input] = character
 end
