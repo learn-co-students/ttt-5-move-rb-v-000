459a6284586163446310a6fdd6d1ac664b04b198
idith almog <almog.idith@gmail.com>
2020-04-13 22:57:05 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -5,5 +5,10 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+def input_to_index(n)
+  return n.to_i-1
+end
 
-# code your input_to_index and move method here!
+def move(array,index, value="X")
+  array[index]=value
+end
